f3c51b52989fe57610ff340387e664f8833a62b0
lemwaizz <mwaidennis26@gmail.com>
2024-05-24 12:11:32 +0200

    complete

--- Merge_sort.py
+++ Merge_sort.py
@@ -1,22 +1,25 @@
 import sys
-lst = [9, 10, 0, 16, -5, 2]
-class merge_sorting:
+
+class MergeSorting:
     @staticmethod
     def merge_sort(lst):
         if len(lst) <= 1:
             return lst
-        middle = len(lst)//2
+        middle = len(lst) // 2
         left = lst[:middle]
         right = lst[middle:]
-        sleft = merge_sort(left)
-        sright = merge_sort(right)
+        
+        sleft = MergeSorting.merge_sort(left)
+        sright = MergeSorting.merge_sort(right)
+        
+        return MergeSorting.merge(sleft, sright)
 
     @staticmethod
     def merge(left, right):
         result = []
         while left and right:
             if left[0] < right[0]:
-                result.append(left[o])
+                result.append(left[0])
                 left.pop(0)
             else:
                 result.append(right[0])
@@ -26,4 +29,11 @@ class merge_sorting:
         if right:
             result += right
         return result
-        print(result)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        lst = list(map(int, sys.argv[1:]))
+        sorted_lst = MergeSorting.merge_sort(lst)
+        print("Sorted List:", sorted_lst)
+    else:
+        print("Please provide a list of integers as command-line arguments.")
