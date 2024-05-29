d25b3e9fa8691541264f04bd8146c5b70e44c5e7
lemwaizz <mwaidennis26@gmail.com>
2024-05-29 15:15:40 +0200

    fixed usage and try to catch errors

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,14 +1,21 @@
 import sys
 """find the nth fibonacci number"""
 class Fibonacci:
+    @staticmethod
     def fibonacci(num):
         if num < 2:
             return num 
         return Fibonacci.fibonacci(num - 1) + Fibonacci.fibonacci(num - 2)
 
-if "__name__" == "__main__":
-    if len(sys.argv) == 1:
-        num = sys.argv[1]
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python scriptname integer")
+        sys.exit(1)
+    try:
+        num = int(sys.argv[1])
+    except ValueError:
+        print("The argument must be an integer.")
+        sys.exit(1)
     print(Fibonacci.fibonacci(num))
 
 
