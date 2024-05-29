ff7c576823af49fcbd1fa83a39046e6a8b735f89
lemwaizz <mwaidennis26@gmail.com>
2024-05-29 15:16:51 +0200

    added documentation

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,7 +1,7 @@
 import sys
 """find the nth fibonacci number"""
 class Fibonacci:
-    @staticmethod
+    @staticmethod #used in place if self is not included in the script 
     def fibonacci(num):
         if num < 2:
             return num 
@@ -9,7 +9,7 @@ class Fibonacci:
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python scriptname integer")
+        print("Usage: python scriptname integer") #used to show how to execute script 
         sys.exit(1)
     try:
         num = int(sys.argv[1])
